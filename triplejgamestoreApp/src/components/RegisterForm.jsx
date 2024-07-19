9746b8e67a563e269fc0618d9f74577585b74e34
Jose Taveras <j.taveras93@yahoo.com>
2024-07-18 21:36:50 -0400

    removed note from Register page

--- triplejgamestoreApp/src/components/RegisterForm.jsx
+++ triplejgamestoreApp/src/components/RegisterForm.jsx
@@ -31,14 +31,7 @@ export default function RegisterForm({setToken}) {
         setError(error.message)
     }
   }
-
-    //   this is the answer to set token not defined but im still confused      
-    // This is a placeholder function. You should implement your own token handling logic.
-    // For example, you might store the token in localStorage, sessionStorage, or state management.
-    //   function setToken(token) {
-    //     localStorage.setItem('authToken', token);
-    //   }
-
+  
   return (
   <>
   <h2>Sign Up.</h2> 
