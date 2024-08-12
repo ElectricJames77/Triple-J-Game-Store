215f9c23638972d39507d42c507037c7340844b5
JakeMcGuire55 <jakemmcguire@gmail.com>
2024-08-12 19:35:08 -0400

    adding formatting

--- triplejgamestoreApp/src/components/GameOverview.jsx
+++ triplejgamestoreApp/src/components/GameOverview.jsx
@@ -1,13 +1,16 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 const GameOverview = () => {
-    const [game, setGame] = useState({})
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const { id } = useParams()
-    const navigate = useNavigate()
+    const [game, setGame] = useState({});
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const { id } = useParams();
+    const [message, setMessage] = useState(null);
+    const navigate = useNavigate();
     const API_URL = 'https://triplej-gamestore-2bf9fca17274.herokuapp.com/api/games' //Games URL
 
     useEffect(() => {
@@ -30,8 +33,8 @@ const GameOverview = () => {
 
     const addToCart = async () => {
         try {
-            const response = await fetch(`${API_URL}/${id}`, {
-                method: "PATCH",
+            const response = await fetch(`https://triplej-gamestore-2bf9fca17274.herokuapp.com/api/cart`, {
+                method: "POST",
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${localStorage.getItem('token')}`
@@ -43,6 +46,8 @@ const GameOverview = () => {
             }
 
             const data = await response.json();
+            setMessage('Added to cart');
+            console.log(data);
             
         } catch (error) {
             console.log(error)
@@ -54,15 +59,26 @@ const GameOverview = () => {
             <h1>{error}</h1>
         ) : game ? (
             <>
-            <div>
-                <div>
-                    <h2>{game?.title}</h2>
-                    <p>{game?.genre}</p>
-                    <p>{game?.description}</p>
+            <div className="singleGameGroup">
+                <div className="image-box">
+                    <img src={game.imageUrl} alt={game.title} style={{ width: "200px", height: "200px"}} />
+                    <h3>Game Name: {game?.title}</h3>
+                    <li>
+                        <ul>Genre: {game?.genre}</ul>
+                        <ul>Price: ${game?.price}</ul>
+                        <ul>Rating: {game.totalRating}</ul>
+                        <ul># of Ratings: {game?.ratingsCount}</ul>
+                    </li>
+                    <p>Description: {game?.description}</p>
+                    <button onClick={addToCart} className="checkoutBtn">Add to Cart</button>
+                    {message && <h1 className="successMessage">{message}</h1>}
+                    <Link to={`/store/`}>
+                        <button>Back To All Games</button>
+                    </Link>
                 </div>
                 
             </div>
-            {/* Design the page for a single game overview with possible recommendations forsimilar games and all game data */}
+            {/* Design the page for a single game overview with possible recommendations for similar games and all game data */}
             </>
         ) : (
             <div>
